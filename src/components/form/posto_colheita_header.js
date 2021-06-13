2ff57aabc77846f322192a643db49aa8c3d6de79
irzelindo <irzelindo.salvador@gmail.com>
2021-06-13 21:22:20 +0200

    Add posto colheita header

---
+++ src/components/form/posto_colheita_header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+// The form seach section
+class PostoColheitaHeader extends Component {
+  render(){
+    return (
+        <div class="row">
+            <div class="text-center col-12 my-4 bg-light">
+                <h6 class="my-4 text-muted"><strong>A PREENCHER PELO POSTO DE COLHEITA</strong></h6> 
+            </div>
+        </div>
+    )
+  }
+}
+
+export default PostoColheitaHeader;
