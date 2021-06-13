751b9ae41781011c840fd12ef5ce15f7e9778c31
irzelindo <irzelindo.salvador@gmail.com>
2021-06-13 21:22:57 +0200

    Add posto colheita to the form

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import FormHeader from "./form_header";
 import FormSeach from './form_search';
+import PostoColheitaHeader from "./posto_colheita_header";
+import PostoColheitaFilters from "./posto_colheita_filters";
 
 class Form extends Component {
   render(){
@@ -8,6 +10,8 @@ class Form extends Component {
         <form>
             <FormHeader />
             <FormSeach />
+            <PostoColheitaHeader />
+            <PostoColheitaFilters />
         </form>
     )
   };
