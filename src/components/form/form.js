ddd30023d2bd961928aee892c0a0cc93a4796a24
irzelindo <irzelindo.salvador@gmail.com>
2021-06-13 19:58:04 +0200

    Add form header

---
+++ src/components/form/form.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Header from "./form_header";
+
+class Form extends Component {
+  render(){
+    return (
+        <form>
+            <Header />
+        </form>
+    )
+  };
+}
+
+export default Form;
---
+++ src/components/form/form_header.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+// The form Title goes in this component
+class Header extends Component {
+  render(){
+    return (
+      <div className="text-center col-12 my-5">
+      <h3 className="my-3 text-muted"><strong>FSR</strong></h3>
+      <h6 className="my-3 text-muted"><strong>FORMULÁRIO DE SOLICITAÇÃO 
+        PARA A DETENCÇÃO QUALITATIVA DO DNA PROVIRAL 
+        DO HIV-1</strong></h6> 
+      </div>
+    )
+  }
+}
+
+export default Header;
