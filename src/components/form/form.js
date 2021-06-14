9bd753811d522d6a90155515f57dcab045f26c51
irzelindo salvador <irzelindo.salvador@gmail.com>
2021-06-14 15:35:45 +0200

    Compile form tree

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -3,6 +3,9 @@ import FormHeader from "./form_header";
 import FormSeach from './form_search';
 import PostoColheitaHeader from "./posto_colheita_header";
 import PostoColheitaFilters from "./posto_colheita_filters";
+import FormRegisto from "./form_registo";
+import FormResultado from "./form_resultado";
+import FormSubmit from "./form_submit";
 
 class Form extends Component {
   render(){
@@ -12,6 +15,9 @@ class Form extends Component {
             <FormSeach />
             <PostoColheitaHeader />
             <PostoColheitaFilters />
+            <FormRegisto />
+            <FormResultado />
+            <FormSubmit />
         </form>
     )
   };
